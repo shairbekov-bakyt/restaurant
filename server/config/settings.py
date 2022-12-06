523ea1589d8bd8a6c39dc095f377328577607a43
c0mrade <shairbekov_bakyt@zeonithub.com>
2022-12-06 15:19:50 +0600

    bugfix/remove_secret_by_from_config

--- server/config/settings.py
+++ server/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2m4pmmltp@&*4o8ayup@krac)hdfhygrzfu5m4v_^i7=sqvjxi'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
